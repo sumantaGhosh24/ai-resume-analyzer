7c62caefa7478352a2b9746e98c5301833a72047
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-05-28 10:17:40 +0530

    update ai model

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,4 +1,5 @@
 import {google} from "@ai-sdk/google";
 
 // export const geminiModel = google("gemini-2.5-flash");
-export const geminiModel = google("gemini-3.1-flash-lite-preview");
+
+export const geminiModel = google("gemini-2.5-flash-lite");
